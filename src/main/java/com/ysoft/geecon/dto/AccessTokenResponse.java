5eecf5ec6d93946d5a59d1ac0da9be6714feb474
Dusan Jakub <dusan.jakub@ysoft.com>
2023-09-15 14:12:14 +0200

    Token response

--- src/main/java/com/ysoft/geecon/dto/AccessTokenResponse.java
+++ src/main/java/com/ysoft/geecon/dto/AccessTokenResponse.java
@@ -1,4 +1,13 @@
 package com.ysoft.geecon.dto;
 
-public record AccessTokenResponse(String tokenType, long expiresIn, String accessToken, String scope, String refreshToken, String idToken) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record AccessTokenResponse(@JsonProperty("token_type") String tokenType,
+                                  @JsonProperty("expires_in") long expiresIn,
+                                  @JsonProperty("access_token") String accessToken,
+                                  String scope,
+                                  @JsonProperty("refresh_token") String refreshToken,
+                                  @JsonProperty("id_token") String idToken) {
 }
